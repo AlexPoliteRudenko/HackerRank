37c8236b2fe2ef9ae3fe7a078acd94c1e4497ba8
AlexPoliteRudenko <rudenko.lesha@gmail.com>
2016-12-02 11:46:02 +0200

    Some solutions for 30 Days of Code Day 22

---
+++ src/Challenge30DaysOfCode/Day22_BinarySearchTree/Node.java
@@ -0,0 +1,10 @@
+package Challenge30DaysOfCode.Day22_BinarySearchTree;
+
+public class Node {
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
